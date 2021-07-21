cb62b3b500717985ec6506ef1b23b0e49ae9d3dc
Joshua Lokken <joshualokken@pm.me>
2021-07-21 13:50:35 -0500

    Finished MDN EmberJS Todos tutorial app

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -19,6 +19,7 @@ module.exports = function (defaults) {
   // modules that you would like to import into your application
   // please specify an object with the list of modules as keys
   // along with the exports of each module as its value.
-
+  app.import('node_modules/todomvc-common/base.css');
+  app.import('node_modules/todomvc-app-css/index.css');
   return app.toTree();
 };
